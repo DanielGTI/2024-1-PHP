95261c25522fd3adfc0f03a621e38695d2c5608f
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2024-04-05 00:07:09 +0000

    Fim de linha

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+
+
 <?php 
 
 	function sortear_Megasena(){
@@ -12,26 +15,30 @@
 	function imprimir_Megasena($numeros){
 		echo "Os números sorteados são: ";
 		for($i = 0; $i < 6; $i++){
-			echo $numeros[$i] . " - ";
+			echo $numeros[$i]; 
+				if( $i != 5)
+					echo " - ";
 		}
-		echo "<br>";
+		echo "<br><br>";
 	}
 	function imprimir_Sorteio($numeros, $n){
 		echo "Números escolhidos: ";
 		for($i = 0; $i < $n; $i++){
-			echo $numeros[$i] . " . ";
+			echo $numeros[$i];
+			if( $i != $n-1)
+				echo " - ";
 		}
-		echo "<br>";
+		echo "<br><br>";
 	}
 
 	function escolha_Usuario($numero_Usuario, $numeros_Sorteados, $n){
 		$acerto = 0;
 		for($i = 0; $i < 6; $i++){
 			for($j = $i; $j < $n; $j++){
-				
+
 				if( $numero_Usuario[$j] == $numeros_Sorteados[$i] ){
 					$acerto++;
-					
+
 				}
 			}
 		}
@@ -39,7 +46,7 @@
 	}
 
 
-		
+
 	$sorteio = sortear_Megasena();
 	$numeros_Usuario = array(3,15,23,45,56,58,10,21,7,18,25,33,31,42,47,48,51,59);
 	$quantidade_numeros = count($numeros_Usuario);
@@ -49,7 +56,5 @@
 	imprimir_Megasena($sorteio);
 
 	echo '<br>ACERTOS = ' . escolha_Usuario($numeros_Usuario, $sorteio, $quantidade_numeros);
-	
+
 ?> 
-
-
